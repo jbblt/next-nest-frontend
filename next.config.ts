01369dd397dba48d942b19bc8b627be76f6dff6f
jbbalmontO2 <132301654+jbbalmont@users.noreply.github.com>
2025-04-07 08:52:50 -0400

    Chore: Fulfilled folder with project

---
+++ next.config.ts
@@ -0,0 +1,27 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  experimental: {
+    turbo: {
+      minify: true,
+      treeShaking: true,
+    }
+  },
+  webpack: (config, { isServer }) => {    if (!isServer) {
+      config.externals.push('fs', 'path', 'os');
+    }
+
+    config.module.rules.push({
+      test: /backend/,
+      use: 'null-loader',
+    });
+
+    return config;
+  },
+};
+
+
+export default nextConfig;
